896f5ffa5132c928626fc8d1f1ff5e61b725f548
Raymond SO <raymond.wc.so@gmail.com>
2021-03-17 21:20:22 +0800

    initial version

---
+++ event.js
@@ -0,0 +1,42 @@
+// truffle exec --network ganache SimpleStorage
+
+module.exports = async function main(callback) {
+    try {
+        const accounts = await web3.eth.getAccounts()
+        const Contract = artifacts.require(process.argv[6])
+        const instance = await Contract.deployed()
+
+        let currentBlockNumber = await web3.eth.getBlockNumber()
+        console.log(`Current block number: ${currentBlockNumber}`)
+
+        // from block 0 to current block
+        for (let block = 0; block <= currentBlockNumber; block++) {
+            // get the numner of transaction in a block
+            console.log(`Block [${block}]`)
+            let nTx = await web3.eth.getBlockTransactionCount(block)
+            console.log(`Number of transaction(s) in block [${block}]: ${nTx}`)
+            // get each transaction in a block
+            for (let i = 0; i < nTx; i++) {
+                let tx = await web3.eth.getTransactionFromBlock(block, i)
+                // console.log(tx)
+                let txReceipt = await web3.eth.getTransactionReceipt(tx.hash)
+                // console.log(txReceipt)
+                for (log of txReceipt.logs) {
+                    for (f of Contract.abi) {
+                        if (f.signature === log.topics[0]) {  // match `topics' against abi
+                            let event = await web3.eth.abi.decodeLog(
+                                f.inputs, log.data, log.topics
+                            )
+                            console.log(`Name: '${f.name}', type: ${f.type}`)
+                            console.log(event)
+                        }
+                    }
+                }
+            }
+        }
+        callback(0);
+    } catch (error) {
+        console.error(error);
+        callback(1);
+    }
+}
